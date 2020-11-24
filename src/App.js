122aa20a1e5cdd372d7fd3622cc78eee7f583b18
Michael Cote <michaelcote7@outlook.com>
2020-11-23 22:42:11 -0800

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      {/*  */}
+    <div className="App container-fluid center_all full_screen">
+      <p>hello</p>
     </div>
   );
-}
+};
 
 export default App;
